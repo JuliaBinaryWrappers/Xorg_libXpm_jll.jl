3fbd0918ace0b6f2d525338de4650bb4e025d123
jlbuild <juliabuildbot@gmail.com>
2026-03-21 02:18:13 +0000

    Xorg_libXpm_jll build 3.5.18+0

--- src/Xorg_libXpm_jll.jl
+++ src/Xorg_libXpm_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Xorg_libXpm")
-JLLWrappers.@generate_main_file("Xorg_libXpm", UUID("1a3ddb2d-74e3-57f3-a27b-e9b16291b4f2"))
+JLLWrappers.@generate_main_file("Xorg_libXpm", Base.UUID("1a3ddb2d-74e3-57f3-a27b-e9b16291b4f2"))
 end  # module Xorg_libXpm_jll
